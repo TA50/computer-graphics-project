04726d722fbe900babbffe7bc0e595d3dadb49c5
Awad Osman <you@example.com>
2024-07-08 17:02:09 +0200

    init

---
+++ build-shaders.bat
@@ -0,0 +1,23 @@
+glslc shaders/BlinnShader.frag -o shaders/BlinnFrag.spv
+glslc shaders/BlinnShader.vert -o shaders/BlinnVert.spv
+
+@REM "shaders/EmissionVert.spv", "shaders/EmissionFrag.spv
+glslc shaders/EmissionShader.frag -o shaders/EmissionFrag.spv
+glslc shaders/EmissionShader.vert -o shaders/EmissionVert.spv
+
+@REM  "shaders/SkyBoxVert.spv", "shaders/SkyBoxFrag.spv",
+
+glslc shaders/SkyBoxShader.frag -o shaders/SkyBoxFrag.spv
+glslc shaders/SkyBoxShader.vert -o shaders/SkyBoxVert.spv
+
+@REM "shaders/NormalMapVert.spv", "shaders/NormalMapFrag.spv"
+glslc shaders/NormalMapShader.frag -o shaders/NormalMapFrag.spv
+glslc shaders/NormalMapShader.vert -o shaders/NormalMapVert.spv
+
+@REM "shaders/TextShader.
+
+glslc shaders/TextShader.frag -o shaders/TextFrag.spv
+glslc shaders/TextShader.vert -o shaders/TextVert.spv
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "A10.hpp"
+
+int main()
+{
+    A10 app;
+
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
